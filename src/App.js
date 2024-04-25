67a8f13a9c677d77e910833799d23cf98e87dc22
Shuja Ur Rehman <sabeeharif@hotmail.com>
2024-04-25 16:23:58 +0500

    initialize project, Redux, tailwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { decrement, increment, selectCount } from "./redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
 
 function App() {
+  const count = useSelector(selectCount);
+  const dispatch = useDispatch();
   return (
     <div className="App">
+      <button
+        className="bg"
+        aria-label="Increment value"
+        onClick={() => dispatch(increment())}
+      >
+        +
+      </button>
+
+      <span>{count}</span>
+
+      <button
+        className="text-red"
+        aria-label="Decrement value"
+        onClick={() => dispatch(decrement())}
+      >
+        -
+      </button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
