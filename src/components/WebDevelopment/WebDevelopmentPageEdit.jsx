a92f3a27aa9a7d076289c095d55d35512beec784
Shuja Ur Rehman <shuja0094@gmail.com>
2024-05-07 16:47:08 +0500

    tech add

--- src/components/WebDevelopment/WebDevelopmentPageEdit.jsx
+++ src/components/WebDevelopment/WebDevelopmentPageEdit.jsx
@@ -8,6 +8,7 @@ import { useNavigate } from "react-router-dom";
 
 export default function WebDevelopmentPageEdit() {
   const [isLoading, setIsLoading] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
     description:
@@ -23,6 +24,14 @@ export default function WebDevelopmentPageEdit() {
       { name: "React", type: "Frontend", img: "react.png" },
       { name: "Node.js", type: "Backend", img: "nodejs.png" },
     ],
+  });
+
+  const [techData, setTechData] = useState({
+    techStack: {
+      name: "",
+      type: "",
+      img: null, // Assuming img is initially null
+    },
   });
   const cardLabels = [
     "User-Centric Design",
@@ -45,20 +54,57 @@ export default function WebDevelopmentPageEdit() {
       techStack: updatedTechStack,
     });
   };
-  const handleImageChange = (e, index) => {
-    const file = e.target.files[0];
-    const updatedTechStack = [...formData.techStack];
-    updatedTechStack[index]["img"] = URL.createObjectURL(file);
-    setFormData({
-      ...formData,
-      techStack: updatedTechStack,
-    });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setTechData((prevFormData) => ({
+      ...prevFormData,
+      techStack: {
+        ...prevFormData.techStack,
+        [name]: value,
+      },
+    }));
   };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setTechData((prevFormData) => ({
+      ...prevFormData,
+      techStack: {
+        ...prevFormData.techStack,
+        img: file,
+      },
+    }));
+  };
+
+  // const handleImageChange = (e, index) => {
+  //   const file = e.target.files[0];
+  //   const updatedTechStack = [...formData.techStack];
+  //   updatedTechStack[index]["img"] = URL.createObjectURL(file);
+  //   setFormData({
+  //     ...formData,
+  //     techStack: updatedTechStack,
+  //   });
+  // };
+
   const handleAddTechStack = () => {
-    setFormData({
-      ...formData,
-      techStack: [...formData.techStack, { name: "", type: "", img: "" }],
-    });
+    // console.log("new", formData);
+    // setFormData({
+    //   ...formData,
+    //   techStack: [...formData.techStack, { name: "", type: "", img: "" }],
+    // });
+    const newTechStackItem = {
+      name: techData.techStack.name,
+      type: techData.techStack.type,
+      img: techData.techStack.img,
+    };
+
+    // Add the new object to the techStack array
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      techStack: [...prevFormData.techStack, newTechStackItem],
+    }));
+    closeModal();
   };
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -75,6 +121,9 @@ export default function WebDevelopmentPageEdit() {
     // dispatch(LandingPageEdit(formData));
     // navigate("/landingPage");
   };
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
 
   return (
     <div className="w-full mb-6">
@@ -99,15 +148,15 @@ export default function WebDevelopmentPageEdit() {
             placeholder="Custom Service Description"
           />
           <div className="mt-4">
-          <label className="text-webDescrip font-semibold">Proposition</label>
-          <textarea
-            className="bg-white shadow-lg text-webDescrip px-3 text-[16px] border focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            name="Proposition"
-            id="Proposition"
-            onChange={handleChange}
-            value={formData.Proposition}
-            placeholder="Custom Service Proposition"
-          />
+            <label className="text-webDescrip font-semibold">Proposition</label>
+            <textarea
+              className="bg-white shadow-lg text-webDescrip px-3 text-[16px] border focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              name="Proposition"
+              id="Proposition"
+              onChange={handleChange}
+              value={formData.Proposition}
+              placeholder="Custom Service Proposition"
+            />
           </div>
           <div className="border border-dashed border-custom-purple p-4 mt-6 ">
             <label className="text-webDescrip font-semibol text-[20px] mx-auto">
@@ -123,12 +172,117 @@ export default function WebDevelopmentPageEdit() {
           </div>
 
           {/* Tech Stack */}
-          <TechStack
+          {/* <TechStack
             techStack={formData.techStack}
             onChange={handleChange}
             onImageChange={handleImageChange}
             onAddTechStack={handleAddTechStack}
-          />
+          /> */}
+
+          <div className="border border-dashed flex flex-col border-custom-purple p-4 mt-6">
+            <label className="text-webDescrip font-semibol text-[20px] ">
+              Tech Stack
+            </label>
+            <div className="w-full flex justify-end">
+              <button
+                type="button"
+                onClick={openModal}
+                className="text-white text-[16px] px-4 py-2 bg-blue-500 rounded-full  hover:bg-blue-600"
+              >
+                Add Tech Stack
+              </button>
+            </div>
+            {console.log("formData", formData?.techStack)}
+            <div className="w-full text-webDescrip font-semibold">Frontend</div>
+            {formData?.techStack?.map((item) =>
+              item?.type === "Frontend" ? (
+                <div className="w-full flex" key={item.name}>
+                  <div className="w-full flex justify-between">
+                    <div>{item?.name}</div>
+                    <div>
+                      <span>Edit</span>
+                      <span>Delete</span>
+                    </div>
+                  </div>
+                </div>
+              ) : null
+            )}
+            {/* Backend */}
+            <div className="w-full text-webDescrip font-semibold">Backend</div>
+            {formData?.techStack?.map((item) =>
+              item?.type === "Backend" ? (
+                <div className="w-full flex" key={item.name}>
+                  <div className="w-full flex justify-between">
+                    <div>{item?.name}</div>
+                    <div>
+                      <span>Edit</span>
+                      <span>Delete</span>
+                    </div>
+                  </div>
+                </div>
+              ) : null
+            )}
+            {/* Database */}
+            <div className="w-full text-webDescrip font-semibold">Database</div>
+            {formData?.techStack?.map((item) =>
+              item?.type === "Database" ? (
+                <div className="w-full flex" key={item.name}>
+                  <div className="w-full flex justify-between">
+                    <div>{item?.name}</div>
+                    <div>
+                      <span>Edit</span>
+                      <span>Delete</span>
+                    </div>
+                  </div>
+                </div>
+              ) : null
+            )}
+            {/* CI/CD */}
+            <div className="w-full text-webDescrip font-semibold">CI/CD</div>
+            {formData?.techStack?.map((item) =>
+              item?.type === "CI/CD" ? (
+                <div className="w-full flex" key={item.name}>
+                  <div className="w-full flex justify-between">
+                    <div>{item?.name}</div>
+                    <div>
+                      <span>Edit</span>
+                      <span>Delete</span>
+                    </div>
+                  </div>
+                </div>
+              ) : null
+            )}
+            {/* VCS */}
+            <div className="w-full text-webDescrip font-semibold">VCS</div>
+            {formData?.techStack?.map((item) =>
+              item?.type === "VCS" ? (
+                <div className="w-full flex" key={item.name}>
+                  <div className="w-full flex justify-between">
+                    <div>{item?.name}</div>
+                    <div>
+                      <span>Edit</span>
+                      <span>Delete</span>
+                    </div>
+                  </div>
+                </div>
+              ) : null
+            )}
+            {/* Testing */}
+            <div className="w-full text-webDescrip font-semibold">Testing</div>
+            {formData?.techStack?.map((item) =>
+              item?.type === "Testing" ? (
+                <div className="w-full flex" key={item.name}>
+                  <div className="w-full flex justify-between">
+                    <div>{item?.name}</div>
+                    <div>
+                      <span>Edit</span>
+                      <span>Delete</span>
+                    </div>
+                  </div>
+                </div>
+              ) : null
+            )}
+          </div>
         </div>
 
         {/* Submit button */}
@@ -153,6 +307,76 @@ export default function WebDevelopmentPageEdit() {
           <ToastContainer />
         </div>
       </form>
+
+      {/* Technology Modal */}
+      {isModalOpen && (
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-700 bg-opacity-50">
+          <div className="bg-white border border-dashed flex flex-col border-custom-purple p-4 mt-6">
+            <label className="text-webDescrip font-semibol text-[20px] ">
+              Tech Stack
+            </label>
+            <div className="mb-4">
+              <label className="text-webDescrip font-semibold">Tech Name</label>
+              <input
+                type="text"
+                name="name"
+                value={formData?.techStack?.name}
+                onChange={handleInputChange}
+                placeholder="Tech Name"
+                className="bg-white shadow-lg text-webDescrip px-3 text-[16px] border focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              />
+              <div className="mb-4">
+                <label className="text-webDescrip font-semibold">
+                  Tech Type
+                </label>
+                <select
+                  name="type"
+                  value={formData?.techStack?.type}
+                  onChange={handleInputChange}
+                  className="bg-white shadow-lg text-webDescrip px-3 text-[16px] border focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                >
+                  <option value="">Select Tech Type</option>
+                  <option value="Frontend">Frontend Technologies</option>
+                  <option value="Backend">Backend Technologies</option>
+                  <option value="Database">Database</option>
+                  <option value="CI/CD">CI/CD tools</option>
+                  <option value="VCS">Version Control Systems</option>
+                  <option value="Testing">Testing tools</option>
+                </select>
+              </div>
+              <div className="mt-4">
+                <label className="text-webDescrip font-semibold mt-2 block">
+                  Upload Image
+                </label>
+                <input
+                  type="file"
+                  name="img"
+                  onChange={handleImageChange}
+                  accept="image/*"
+                  className="bg-white shadow-lg text-webDescrip px-3 text-[16px] border focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                />
+              </div>
+            </div>
+            <div>
+              <button
+                type="button"
+                onClick={handleAddTechStack}
+                className="text-white text-[16px] px-4 py-2 bg-blue-500 rounded-full  hover:bg-blue-600"
+              >
+                Save
+              </button>
+
+              <button
+                type="button"
+                onClick={closeModal}
+                className="bg-gray-300 text-gray-700 px-4 py-2 rounded-full hover:bg-gray-400"
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
