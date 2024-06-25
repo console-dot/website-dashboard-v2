721a8fb87002086fa05491d726888e27e66a10c6
Shuja Ur Rehman <shuja0094@gmail.com>
2024-06-25 18:17:06 +0500

    Update OpenPositionsPage.jsx

--- src/components/OpenPositions/OpenPositionsPage.jsx
+++ src/components/OpenPositions/OpenPositionsPage.jsx
@@ -60,7 +60,6 @@ export const OpenPositionsPage = ({ setIsValid, isValid }) => {
     }
   }, [location.pathname, isValid]);
 
-
   // Fetch hero description data
   useEffect(() => {
     getHeroDescription()
@@ -173,7 +172,6 @@ export const OpenPositionsPage = ({ setIsValid, isValid }) => {
       alert("Failed to update hero description");
     }
   };
-
 
   return (
     <>
@@ -336,30 +334,30 @@ export const OpenPositionsPage = ({ setIsValid, isValid }) => {
         <div className="flex flex-col w-full">
           {/* hero description */}
           <div className="flex flex-col" style={{ width: "70%" }}>
-                    <label className="" style={{ color: "grey" }}>
-                      Hero Description
-                    </label>
-                    <div className="flex flex-row gap-2">
-                      <textarea
-                        className="bg-white shadow-lg text-webDescrip px-3 text-[16px] border focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        type="text"
-                        name="openPositionHero"
-                        id="openPositionHero"
-                        onChange={(e) => setopenPositionHero(e.target.value)}
-                        value={openPositionHero}
-                        placeholder="Hero Description"
-                      />
-                      <button
-                        className="bg-blue-500 text-white py-1 px-6 rounded-lg"
-                        type="button"
-                        onClick={handleHeroDescription}
-                      >
-                        Update
-                      </button>
-                    </div>
-                    <div className="border-b border-solid border-custom-purple mt-2"></div>
-                  </div>
-          {positions.map((item) => (
+            <label className="" style={{ color: "grey" }}>
+              Hero Description
+            </label>
+            <div className="flex flex-row gap-2">
+              <textarea
+                className="bg-white shadow-lg text-webDescrip px-3 text-[16px] border focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                type="text"
+                name="openPositionHero"
+                id="openPositionHero"
+                onChange={(e) => setopenPositionHero(e.target.value)}
+                value={openPositionHero}
+                placeholder="Hero Description"
+              />
+              <button
+                className="bg-blue-500 text-white py-1 px-6 rounded-lg"
+                type="button"
+                onClick={handleHeroDescription}
+              >
+                Update
+              </button>
+            </div>
+            <div className="border-b border-solid border-custom-purple mt-2"></div>
+          </div>
+          {positions?.map((item) => (
             <div key={item._id}>
               <OpenPositionsCard
                 data={item}
